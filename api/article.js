8073068951c5be728a289b9849fb64f3f41612f6
trJoshuaCamon <joshua.camon@tooltwist.com>
2025-04-08 17:03:22 +0800

    added api folder

---
+++ api/article.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
+const { Readability } = require("@mozilla/readability");
+
+module.exports = async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) return res.status(400).json({ error: "URL is required" });
+
+  try {
+    const articlePage = await axios.get(url);
+    const dom = new JSDOM(articlePage.data, { url });
+
+    const article = new Readability(dom.window.document).parse();
+
+    return res.status(200).json({
+      title: article.title,
+      content: article.textContent,
+    });
+  } catch (error) {
+    console.error("Error fetching article:", error.message);
+    return res.status(500).json({ error: "Failed to fetch full article" });
+  }
+};
---
+++ api/news/[category].js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  const {
+    query: { category },
+  } = req;
+
+  const validCategories = [
+    "topHeadlines",
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
+
+  if (!validCategories.includes(category)) {
+    return res.status(400).json({ error: "Invalid category" });
+  }
+
+  try {
+    const apiKey = process.env.NEWS_API_KEY;
+    const baseUrl =
+      category === "topHeadlines"
+        ? `https://newsapi.org/v2/top-headlines?country=us&pageSize=100&apiKey=${apiKey}`
+        : `https://newsapi.org/v2/top-headlines?category=${category}&country=us&pageSize=100&apiKey=${apiKey}`;
+
+    const response = await axios.get(baseUrl);
+    return res.status(200).json(response.data.articles);
+  } catch (error) {
+    console.error("Error fetching category:", error.message);
+    return res.status(500).json({ error: "Failed to fetch news" });
+  }
+};
